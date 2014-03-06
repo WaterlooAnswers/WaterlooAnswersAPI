d7d77d22ca0dbd31764eb5f20306ccb1019f9772
Sahil Jain <sahil.jain@live.ca>
2014-03-05 20:53:13 -0500

    capitalized firstname, changed like icons

--- app/routes.js
+++ app/routes.js
@@ -88,6 +88,7 @@ module.exports = function(app, passport){
           
         });
 
+
         app.post('/signup', passport.authenticate('local-signup', {
           successRedirect : '/loginsuccess', // redirect to the secure profile section
           failureRedirect : '/signup', // redirect back to the signup page if there is an error
