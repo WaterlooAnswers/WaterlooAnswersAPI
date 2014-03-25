241d6332526686c1538fda45c332c26c3e4e8f57
Sahil Jain <sahil.jain@live.ca>
2014-03-24 22:00:50 -0400

    fixed signup success redirect

--- app/routes.js
+++ app/routes.js
@@ -97,7 +97,7 @@ Question.schema.path('category').enumValues.forEach(function(entry){
 
 
 app.post('/signup', passport.authenticate('local-signup', {
-          successRedirect : '/home', // redirect to the secure profile section
+          successRedirect : '/', // redirect to the secure profile section
           failureRedirect : '/signup', // redirect back to the signup page if there is an error
           failureFlash : true // allow flash messages
         }));
